4bc32e0214ca633000de17e2d6330c4b7b561720
Zumi Daxuya <daxuya.zumi+code@protonmail.com>
2020-06-27 23:35:46 +0700

    translation spam

---
+++ app/translations/es_ES.ts
@@ -0,0 +1,182 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="es_ES" sourcelanguage="en_US">
+<context>
+    <name>Game</name>
+    <message>
+        <location filename="../game/game.ui" line="14"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../game/game.ui" line="52"/>
+        <source>Currently playing: Empty</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../game/game.ui" line="84"/>
+        <source>Dark: 99</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../game/game.ui" line="91"/>
+        <source>Light: 99</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../game/game.ui" line="105"/>
+        <location filename="../game/game.cpp" line="162"/>
+        <source>No more moves left!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../game/game.ui" line="98"/>
+        <source>Time: 00:00</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../game/game.cpp" line="153"/>
+        <source>Dark: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../game/game.cpp" line="154"/>
+        <source>Light: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../game/game.cpp" line="206"/>
+        <source>Time: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../game/game.cpp" line="243"/>
+        <source>Dark</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../game/game.cpp" line="246"/>
+        <source>Light</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../game/game.cpp" line="249"/>
+        <source>Empty</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../game/game.cpp" line="252"/>
+        <source>Currently playing: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>GameOverScreen</name>
+    <message>
+        <location filename="../over/gameoverscreen.ui" line="14"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../over/gameoverscreen.ui" line="25"/>
+        <source>Game Over</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../over/gameoverscreen.ui" line="41"/>
+        <source>New Game</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../over/gameoverscreen.ui" line="48"/>
+        <source>Game Time: 00:00</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../over/gameoverscreen.ui" line="110"/>
+        <source>And the winner is Empty, with a total of 99 tiles!</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../mainWindow.ui" line="14"/>
+        <source>FlipTiles</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainWindow.ui" line="107"/>
+        <source>Flip Tiles</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainWindow.ui" line="151"/>
+        <source>File Bug</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainWindow.ui" line="160"/>
+        <source>Sources</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainWindow.ui" line="169"/>
+        <source>About</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainWindow.ui" line="178"/>
+        <source>Exit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainWindow.ui" line="181"/>
+        <source>Ctrl+Q</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="62"/>
+        <source>Empty</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="62"/>
+        <source>Dark</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="62"/>
+        <source>Light</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="80"/>
+        <source>Help</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="134"/>
+        <source>And the winner is %1, with a total of %2 tiles!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="135"/>
+        <source>Game Time: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QApplication</name>
+    <message>
+        <location filename="../main.cpp" line="71"/>
+        <source>Reversi/Othello Game</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="83"/>
+        <source>Git version %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
